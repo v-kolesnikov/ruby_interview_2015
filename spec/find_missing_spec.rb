1dd8dc469fc23989aa7cff5fead857c676538121
kolesnikov <re.vkolesnikov@gmail.com>
2015-09-16 11:24:46 +0300

    Fix find_missing.rb
     * Fix algorithm for the case of two consecutive missed numbers
     * Add the offset as function parameter
     * Add the appropriate tests

--- spec/find_missing_spec.rb
+++ spec/find_missing_spec.rb
@@ -4,5 +4,14 @@ describe "find_missing" do
   it { expect(find_missing([])).to eq [] }
   it { expect(find_missing([1, 2, 3, 4])).to eq [] }
   it { expect(find_missing([1, 2, 3, 5])).to eq [4] }
-  it { expect(find_missing([1, 3, 4, 5, 6, 8])).to eq [2, 7] }
+  it { expect(find_missing([1, 3, 4, 6])).to eq [2, 5] }
+  it { expect(find_missing([1, 2, 3, 6])).to eq [4, 5] }
+  it { expect(find_missing([1, 4, 5, 8])).to eq [2, 3, 6, 7] }
+  it { expect(find_missing([3, 4, 5, 8])).to eq [1, 2, 6, 7] }
+
+  context "with offset" do
+    it { expect(find_missing([4, 5, 6, 8], 4)).to eq [7] }
+    it { expect(find_missing([4, 5, 6, 9], 4)).to eq [7, 8] }
+    it { expect(find_missing([4, 7, 8, 11], 4)).to eq [5, 6, 9, 10] }
+  end
 end
--- src/find_missing.rb
+++ src/find_missing.rb
@@ -1,12 +1,9 @@
-def find_missing(a)
-  offset = 1
-  missing = []
-  a.sort.each_with_index do |item, index|
+def find_missing(a, offset = 1)
+  a.sort.each.with_index.with_object([]) do |(item, index), missing|
     diff = item - index
     if diff > offset
-      missing << item - 1
+      (diff - offset).downto(1).each { |d| missing << item - d }
       offset = diff
     end
   end
-  missing
 end
